1c8dab3dfaefd0a85063fa1c407a0165967ad399
clementbriens <50340519+clementbriens@users.noreply.github.com>
2022-02-20 13:27:06 +0100

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -52,12 +52,12 @@ class TwitterIngest():
                 for index, row in df.iterrows():
                     id = self.hash_string("{}:{}".format(file, index))
                     for key in row.keys():
-                        if 'count' in key and 'date' not in key and 'account' not in key:
+                        if ('count' in key or 'id' in 'key') and 'date' not in key and 'account' not in key and "=" not in row[key]:
                             if row[key] == ' ':
                                 row[key] = None
                             else:
                                 row[key] = int(row[key])
-                        if 'date' in key:
+                        if 'date' in key or 'time' in key:
                             row[key] = dateparser.parse(row[key])
                     self.es.index(index_name, id = id, body = dict(row))
                     print(index_name, index, '/', len(df), end = '\r')
